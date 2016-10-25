c16434a45abf3129331498b8f13ac83f665d15fe
Seungjae Moon <sjaymoon15@gmail.com>
2016-10-25 14:11:22 -0400

    add dropdown directive

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
 
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
+	items: Ingredient[]= [
+		new Ingredient('onion', 10),
+		new Ingredient('brie' , 20)
+	]
 
   constructor() { }
 
